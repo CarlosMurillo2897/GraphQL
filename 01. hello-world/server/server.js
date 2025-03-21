3d1402b1b22c9e244b95db98c3f7873d7bfe4e1e
Murillo Carlos <cmb2897@gmail.com>
2025-03-21 00:02:04 -0600

    Initializing 01 Section Hello World., performing Apollo Server and Client consumming dynamic.

---
+++ 01. hello-world/server/server.js
@@ -0,0 +1,22 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const typeDefs = `
+    schema {
+        query: Query
+    }
+
+    type Query {
+        greeting: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        greeting: () => 'Hello World!'
+    },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const { url } = await startStandaloneServer(server, { listen: { port: 9000 }});
+console.log(`🚀 Server ready at ${url}`);
