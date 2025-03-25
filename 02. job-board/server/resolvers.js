58aceec31ca5d934f49d860e10e34a09c478fee2
Murillo Carlos <cmb2897@gmail.com>
2025-03-25 13:50:52 -0600

    Adding error if not found handler for Resolvers.

--- 02. job-board/server/resolvers.js
+++ 02. job-board/server/resolvers.js
@@ -1,10 +1,23 @@
 import { getJobs, getJob, getJobsByCompany } from './db/jobs.js';
 import { getCompany } from './db/companies.js';
+import { GraphQLError } from 'graphql';
 
 export const resolvers = {
     Query: {
-        company: (_root, { id }) => getCompany(id),
-        job: (_root, { id }) => getJob(id),
+        company: async (_root, { id }) => {
+            const company = await getCompany(id);
+            if(!company) {
+                throw notFoundError(`Company with id ${id} not found`);
+            }
+            return company;
+        },
+        job: async (_root, { id }) => {
+            const job = await getJob(id);
+            if(!job) {
+                throw notFoundError(`Job with id ${id} not found`);
+            }
+            return job;
+        },
         jobs: () => getJobs()
     },
 
@@ -17,6 +30,12 @@ export const resolvers = {
         company: (job) => getCompany(job.companyId)
     },
 };
+
+function notFoundError(message) {
+    return new GraphQLError(message, {
+        extensions: { code: 'NOT_FOUND' }
+    });
+}
 
 function toIsoDate(value) {
     return value.slice(0, 'yyyy-mm-dd'.length);
